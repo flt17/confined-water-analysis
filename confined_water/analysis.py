ae9fc8ec23528e12cf2895df701e64c6d4792523
Fabian Thiemann <ucapflt@ucl.ac.uk>
2021-01-19 12:02:59 +0000

    Adding the routine set_sampling_times

--- confined_water/analysis.py
+++ confined_water/analysis.py
@@ -80,6 +80,47 @@ class Simulation:
         # Trajectory, can be one ore multiple. If more than one, first element is centroid.
         # Functions which compute a specific property will choose which universe is picked in case of PIMD.
         if read_positions:
-            self.position_universes = utils.get_mdanalysis_universe(
-                self.directory_path, "positions"
+            universes = utils.get_mdanalysis_universe(self.directory_path, "positions")
+
+            # to make sure self.position_universes is always a list of MDAnalysis Universes
+            self.position_universes = (
+                universes if isinstance(universes, list) == list else [universes]
             )
+
+    def set_sampling_times(
+        self,
+        start_time: int = None,
+        end_time: int = None,
+        frame_frequency: int = None,
+        time_between_frames: int = None,
+    ):
+
+        """
+        Set times for analysis of trajectories.
+        Arguments:
+            start_time (int) : Start time for analysis.
+            end_time (int) : End time for analysis.
+            frame_frequency (int): Take every nth frame only.
+            time_between_frames (int): Time (in fs) between two frames in sampled trajectory, e.g. 100 fs.
+        Returns:
+
+        """
+
+        self.start_time = start_time if start_time is not None else self.start_time
+        self.frame_frequency = (
+            frame_frequency if frame_frequency is not None else self.frame_frequency
+        )
+        self.time_between_frames = (
+            time_between_frames if time_between_frames is not None else self.time_between_frames
+        )
+
+        total_time = (self.position_universes[0].nframes - 1) * self.time_between_frames
+        self.end_time = (
+            total_time if end_time == -1 else end_time if end_time is not None else self.end_time
+        )
+
+        print(f"SUCCESS: New sampling times.")
+        print(f"Start time: \t \t{self.start_time} \t fs")
+        print(f"End time: \t \t{self.end_time} \t fs")
+        print(f"Time between frames: \t{self.time_between_frames} \t fs")
+        print(f"Frame frequency: \t{self.frame_frequency}")
