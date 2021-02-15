77066200c923ca7e7e1f1568b8294b1c5bee9697
Fabian Thiemann <ucapflt@ucl.ac.uk>
2021-02-15 17:17:53 +0000

    Example jupyter notebook for hydrogen bonding

--- confined_water/free_energy.py
+++ confined_water/free_energy.py
@@ -1,4 +1,5 @@
 import numpy as np
+import sys
 from tqdm.notebook import tqdm
 
 sys.path.append("../")
@@ -25,8 +26,11 @@ def compute_atomic_probabilities(
 
     """
 
-    # define dimensions not periodic, indices
-    not_pbc_indices = list(set(pbc_indices) ^ set([0, 1, 2]))
+    if len(pbc_indices) == 1:
+        # define dimensions not periodic, indices (only need for tube)
+        not_pbc_indices = list(set(pbc_indices) ^ set([0, 1, 2]))
+        periodic_vector = np.zeros(3)
+        periodic_vector[pbc_indices] = 1
 
     # loop over all trajectories (only relevant for PIMD):
     for count_universe, universe in enumerate(position_universes):
@@ -55,11 +59,11 @@ def compute_atomic_probabilities(
             translation_from_frame0 = solid_atoms.atoms.positions[10] - anchor_coordinates
             universe.atoms.positions -= translation_from_frame0
 
-            # to enable an easy rotation, translate atoms to COM
-            universe.atoms.positions -= universe.atoms.center_of_mass()
-
             # 2. Rotations (only relevant for nanotubes obviously)
             if len(pbc_indices) == 1:
+
+                # to enable an easy rotation, translate atoms to COM
+                universe.atoms.positions -= universe.atoms.center_of_mass()
 
                 # Compute angle between reference atom and axis perpendicular to periodic axis
                 angle_anchor_first_axis = np.arccos(
@@ -71,4 +75,15 @@ def compute_atomic_probabilities(
                     )
                 )
 
+                # get rotation matrix for periodic axis and computed angle
+                rotation_matrix = utils.rotation_matrix(periodic_vector, -angle_anchor_first_axis)
+
+                # rotate atoms, so that this can be compared
+                universe.atoms.positions = np.matmul(rotation_matrix, universe.atoms.positions.T).T
+
+                # translate now back to center of mass
+                universe.atoms.positions += center_of_mass
+
+                # as we are already in the if loop, we can compute tube diameter and circumference
+                # this saves time for the computation of the profile in case of flat surfaces
                 breakpoint()
